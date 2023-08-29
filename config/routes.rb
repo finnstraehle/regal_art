9deef10f7a515879df894337915fcb4555c809bd
Finn Straehle <finn.straehle1@gmx.ch>
2023-08-29 14:45:37 +0200

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,4 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/dashboard", to: "pages#dashboard", as: :dashboard
   get "/discovery", to: "pages#discovery", as: :discovery
-
-  resources :subscriptions, only: [:index, :create]
-
-  resources :artists, only: [:index, :show] do
-    resources :artworks, only: [:index, :show]
-      resources :art_details, only: [:index, :show]
-  end
-
-  resources :events, only: [:index, :show, :new, :create, :delete, :edit] do
-    resources :event_attendances, only: [:create]
-  end
-
-  resources :conversations, only: :show do
-    resources :messages, only: :create
-  end
 end
