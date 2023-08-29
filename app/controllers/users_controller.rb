46234cee0b42a3e5626625301488149427069007
Finn Straehle <finn.straehle1@gmx.ch>
2023-08-29 15:45:50 +0200

    dashboard and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
-  
+  def index
+    @artists = User.where(is_artist: true)
+  end
+
+  def show
+    @artist = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/dashboard", to: "pages#dashboard", as: :dashboard
   get "/discovery", to: "pages#discovery", as: :discovery
+
+  resources :events, only: %i[index show new create edit update destroy]
+  resources :subscriptions, only: %i[index create destroy]
+  resources :users, only: %i[index show]
 end
