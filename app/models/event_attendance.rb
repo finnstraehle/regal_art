f064a7ec6c168ea149d3c55eea41720124e94769
Finn Straehle <finn.straehle1@gmx.ch>
2023-09-01 12:10:47 +0200

    improved seeds and added images to events

--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -4,4 +4,10 @@ class EventAttendance < ApplicationRecord
 
   validates :user, presence: true
   validates :event, presence: true
+
+  MESSAGES = [
+    "Hello! I would like to make a reservation for two at your art gallery on Wednesday.",
+    "Hi there! I'm interested in reserving a spot for myself and a guest to visit your art gallery. Can you please confirm availability?",
+    "Good day! I'd like to secure a reservation to explore your art gallery. Could you please let me know if that's possible?"
+  ]
 end
