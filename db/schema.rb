c0c78ab00921ff36d69e9fdcf3afe8c43d8f37f8
Finn Straehle <finn.straehle1@gmx.ch>
2023-09-03 02:14:38 +0200

    added artwork map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_162927) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_223428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_162927) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "x_value"
+    t.integer "y_value"
     t.index ["artwork_id"], name: "index_art_details_on_artwork_id"
   end
 
@@ -58,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_162927) do
     t.string "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "has_details"
     t.index ["user_id"], name: "index_artworks_on_user_id"
   end
 
