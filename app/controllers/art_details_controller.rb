cbc9b25cb8a700388ffee3cc05d80d920c5918f3
Finn Straehle <finn.straehle1@gmx.ch>
2023-09-06 19:25:46 +0200

    solved crud canvas

--- app/controllers/art_details_controller.rb
+++ app/controllers/art_details_controller.rb
@@ -1,14 +1,14 @@
 class ArtDetailsController < ApplicationController
   def create
-    @canvas = Artwork.find(params[:art_detail][:artwork_id])
     @art_detail = ArtDetail.new(art_detail_params)
-    @art_detail.save!
-    redirect_to edit_artwork_path(@canvas)
+    @art_detail.artwork = Artwork.find(params[:artwork_id])
+    @art_detail.save
+    redirect_to edit_artwork_path(@art_detail.artwork)
   end
 
   private
 
   def art_detail_params
-    params.require(:art_detail).permit(:artwork_id, :title, :description)
+    params.require(:art_detail).permit(:artwork_id, :title, :description, :x_value, :y_value)
   end
 end
